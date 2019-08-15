1e3b6d7d175118a86437696552daa97b290a6e3c
estebanVazquezGtz <esteban.vazquez@code-ing.com>
2019-08-15 12:29:51 -0500

    Version con soporte para Open rex quad core y solo core

--- lib/asm-offsets.c
+++ lib/asm-offsets.c
@@ -1,17 +1,3 @@
-/*
- * Adapted from Linux v2.6.36 kernel: arch/powerpc/kernel/asm-offsets.c
- *
- * This program is used to generate definitions needed by
- * assembly language modules.
- *
- * We use the technique used in the OSF Mach kernel code:
- * generate asm statements containing #defines,
- * compile this file to assembler, and then extract the
- * #defines from the assembly-language output.
- *
- * SPDX-License-Identifier:	GPL-2.0+
- */
-
 #include <common.h>
 
 #include <linux/kbuild.h>
---
+++ mx6qopenrex_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Aug 14 2019"
+#define U_BOOT_TIME "17:06:07"
+#define U_BOOT_TZ "-0500"
